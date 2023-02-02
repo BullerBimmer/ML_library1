307ffcfa1a5af3def3bc34fa13d44ce9782c6e53
BullerBimmer <emilkdn@gmail.com>
2023-02-02 12:08:02 +0100

    git

--- library/linear_library.py
+++ library/linear_library.py
@@ -21,11 +21,33 @@ class LinearRegression:
         return X @ self.weights + self.bias
 
 
+import numpy as np
 
-X = np.array([[1], [2], [3], [4]])
-y = np.array([2, 4, 6, 8])
+class PolynomialRegression():
+    def __init__(self, degree):
+        self.degree = degree
+        self.weights = None
+        self.bias = None
 
-test = LinearRegression()
-test.fit(X, y, 0.01, 1000)
-t = test.predict(X)
-print(t)
+    def fit(self, X, y, learning_rate, n_iterations):
+        X = self._transform_features(X)
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        
+        for _ in range(n_iterations):
+            y_pred = X @ self.weights + self.bias
+            dw = (1 / n_samples) * X.T @ (y_pred - y)
+            db = (1 / n_samples) * np.sum(y_pred - y)
+            self.weights -= learning_rate * dw
+            self.bias -= learning_rate * db
+
+    def predict(self, X):
+        X = self._transform_features(X)
+        return X @ self.weights + self.bias
+
+    def _transform_features(self, X):
+        X_poly = X.copy()
+        for i in range(2, self.degree + 1):
+            X_poly = np.hstack((X_poly, X ** i))
+        return X_poly
--- test/test_linear_library.py
+++ test/test_linear_library.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from library.linear_library import LinearRegression
+from library.linear_library import LinearRegression, PolynomialRegression
 
 class TestLinearRegression(unittest.TestCase):
     def setUp(self):
@@ -19,6 +19,26 @@ class TestLinearRegression(unittest.TestCase):
         self.model.fit(self.X, self.y, self.learning_rate, self.n_iterations)
         y_pred = self.model.predict(self.X)
         self.assertTrue(np.allclose(y_pred, self.y, 0.1))
+
+
+class TestPolynomialRegression(unittest.TestCase):
+    def setUp(self):
+        self.model = PolynomialRegression(2)
+        self.X = np.array([[1], [2], [3], [4]])
+        self.y = np.array([2, 4, 6, 8])
+        self.learning_rate = 0.01
+        self.n_iterations = 10000
+
+    def test_fit(self):
+        self.model.fit(self.X, self.y, self.learning_rate, self.n_iterations)
+        self.assertIsNotNone(self.model.weights)
+        self.assertIsNotNone(self.model.bias)
+
+    def test_predict(self):
+        self.model.fit(self.X, self.y, self.learning_rate, self.n_iterations)
+        y_pred = self.model.predict(self.X)
+        self.assertTrue(np.allclose(y_pred, self.y, 0.1))
+
 
 if __name__ == '__main__':
     unittest.main()
